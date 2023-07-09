1a3899bd26704ebb2556858fc3238c82105e2dbb
Nazarii Polinyk <nazarii.polinyk@gmail.com>
2023-07-09 23:03:22 +0300

    Add database

---
+++ BookDiaryApplication.Data/BookDiaryDB/Migrations/20230709195200_Initial.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookDiaryApplication.Data.BookDiaryDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "date", nullable: true),
+                    AuthorRef = table.Column<int>(type: "int", nullable: false),
+                    GenreRef = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Book_Author",
+                        column: x => x.AuthorRef,
+                        principalTable: "Author",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Book_Genre",
+                        column: x => x.GenreRef,
+                        principalTable: "Genre",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookReview",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Commentary = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserRef = table.Column<int>(type: "int", nullable: false),
+                    BookRef = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookReview", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookReview_Book",
+                        column: x => x.BookRef,
+                        principalTable: "Book",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BookReview_User",
+                        column: x => x.UserRef,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_AuthorRef",
+                table: "Book",
+                column: "AuthorRef");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_GenreRef",
+                table: "Book",
+                column: "GenreRef");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookReview_BookRef",
+                table: "BookReview",
+                column: "BookRef");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookReview_UserRef",
+                table: "BookReview",
+                column: "UserRef");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookReview");
+
+            migrationBuilder.DropTable(
+                name: "Book");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+        }
+    }
+}
