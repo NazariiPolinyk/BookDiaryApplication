f14be96600a43307cd17b45811c552c13ea10bea
Nazarii Polinyk <nazarii.polinyk@gmail.com>
2023-07-09 22:52:45 +0300

    Make ReleaseDate nullable

--- BookDiaryApplication.Data/BookDiaryDB/Models/Book.cs
+++ BookDiaryApplication.Data/BookDiaryDB/Models/Book.cs
@@ -7,7 +7,7 @@ namespace BookDiaryApplication.Data.BookDiaryDB.Models
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public DateTime ReleaseDate { get; set; }
+    public DateTime? ReleaseDate { get; set; }
     public int AuthorRef { get; set; }
     public int GenreRef { get; set; }
 
